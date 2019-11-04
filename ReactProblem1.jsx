34488607f5c25bb870080ffb72db2285c9d3b1c8
iqbalhapid <iqbalhapid18@gmail.com>
2019-11-04 14:09:28 +0700

    init commit

---
+++ ReactProblem1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import('./HelloComponent.css')
+
+
+const HelloComponent = () => {
+    return <p className='text' >Hello World Component</p>
+} 
+
+export default HelloComponent;
